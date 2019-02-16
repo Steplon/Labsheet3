9466ed5fd48123340a3d049bd78c8496bfcae931
Steplon <43827083+Steplon@users.noreply.github.com>
2019-02-16 16:20:34 +0000

    Exercise 2 finished

---
+++ Exercise2_Members/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise2_Members
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        ObservableCollection<Member> members = new ObservableCollection<Member>();
+
+        ObservableCollection<string> memberTypes = new ObservableCollection<string>(); 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            memberTypes.Add("Full");
+            memberTypes.Add("Off Peak");
+            memberTypes.Add("OAP");
+            memberTypes.Add("Student");
+
+
+            members.Add(new Member("Sally Witmore", "Full", "01/03/2016"));
+            members.Add(new Member("Tom Hardy", "OAP", "01/03/2016"));
+            members.Add(new Member("Mel D", "Student", "01/03/2016"));
+
+            DisplayUpdate();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string name = tbxName.Text;
+            string type = cbxMemberType.Text;
+            string date = dpDateJoined.Text;
+            members.Add(new Member(name,type,date));
+
+            DisplayUpdate();
+        }
+
+        private void DisplayUpdate()
+        {
+            lbxMembers.ItemsSource = members;
+            cbxMemberType.ItemsSource = memberTypes;
+
+            int numMmbers = 0;
+            foreach (Member m in members)
+            {
+                numMmbers++;
+            }
+            txblTotalMembers.Text = $"Total Member: {numMmbers}";
+        }
+    }
+}
---
+++ Exercise2_Members/Member.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2_Members
+{
+    class Member
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime DateJoined { get; set; }
+
+        public Member(string name, string type, string dateJoined)
+        {
+            Name = name;
+            Type = type;
+            DateJoined = DateTime.Parse(dateJoined);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}, {2}", Name, Type, DateJoined.ToShortDateString());
+        }
+
+    }
+}
