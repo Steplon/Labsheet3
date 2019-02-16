f796d175cac9ef2d4a54c3ccaeaa6aa7f94b4e0f
Steplon <43827083+Steplon@users.noreply.github.com>
2019-02-16 15:06:20 +0000

    Exercise 1 Finished

---
+++ Exercise1_Expenses/Expenses.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1_Expenses
+{
+    class Expenses
+    {
+
+        public string Category { get; set; }
+        public decimal Cost { get; set; }
+        public DateTime DateOccured { get; set; }
+
+        public Expenses()
+        {
+
+        }
+
+        public Expenses(string category, decimal cost, DateTime dateOccured)
+        {
+            Category = category;
+            Cost = cost;
+            DateOccured = dateOccured;
+        }
+
+        public override string ToString()
+        {
+            return $"{Category} for {Cost:C} on {DateOccured.ToShortDateString()} ";
+        }
+
+    }
+}
--- Exercise1_Expenses/MainWindow.xaml.cs
+++ Exercise1_Expenses/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,54 @@ namespace Exercise1_Expenses
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        ObservableCollection<Expenses> expenses = new ObservableCollection<Expenses>();
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            expenses.Add(new Expenses("Travel", 100m, DateTime.Parse("04/09/2018")));
+            expenses.Add(new Expenses("Fuel", 19.32m, DateTime.Parse("09/12/2018")));
+            expenses.Add(new Expenses("Stationary", 10.04m, DateTime.Parse("24/12/2018")));
+
+            DisplayExpenses();
+        }
+
+        private void BtnAddExpense_Click(object sender, RoutedEventArgs e)
+        {
+            expenses.Add(new Expenses("Food", 294.44m, DateTime.Parse("09/09/2018")));
+            DisplayExpenses();
+        }
+
+        private void BtnRemoveExpense_Click(object sender, RoutedEventArgs e)
+        {
+            expenses.RemoveAt(expenses.Count-1);
+            DisplayExpenses();
+        }
+
+
+        private decimal GetTotalExpenses()
+        {
+            decimal total = 0;
+
+            foreach (Expenses e in expenses)
+            {
+                total += e.Cost;
+            }
+
+            return total;
+        }
+
+        private void DisplayExpenses()
+        {
+            txblTotalExpenses.Text = $"Total Expenses: {Convert.ToDecimal(GetTotalExpenses()):C}";
+            lbxExpenses.ItemsSource = expenses;
+
         }
     }
 }
